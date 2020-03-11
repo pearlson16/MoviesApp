85bd23411015a29d8302ba10454f145fa6c5baf1
Power Moves Dev <powermovesdev@gmail.com>
2020-03-11 14:26:10 -0700

    Initial build

---
+++ app/src/main/java/com/example/movieapp/adapter/MoviesAdapter.java
@@ -0,0 +1,65 @@
+package com.example.movieapp.adapter;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.movieapp.R;
+import com.example.movieapp.model.Movie;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private Configuration mContext;
+    private List<Movie> movieList;
+
+    public MoviesAdapter(Context mContext, List<Movie> movieList){
+        this.mContext = mContext;
+        this.movieList = movieList;
+    }
+
+    @Override
+    public MoviesAdapter.MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.movie_card, viewGroup, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MoviesAdapter.MyViewHolder viewHolder, int i){
+        viewHolder.title.setText(movieList.get(i).getOriginalTitle());
+        String vote = Double.toString(movieList.get(i).getVoteAverage());
+        viewHolder.userrating.setText(vote);
+
+        Glide.with(mContext)
+                .load(movieList.get(i).getPosterPath())
+                .placeholder(R.drawable.load)
+                .into(viewHolder.thumbnail);
+    }
+
+    @Override
+    public int getItemCount(){
+        return movieList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        public TextView title, userrating;
+        public ImageView thumbnail;
+
+
+        public MyViewHolder(View view){
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            userrating = (TextView) view.findViewById(R.id.userrating);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+        }
+    }
+}
